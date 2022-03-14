100f3bd49b3e6f06252849a611d6cf721b659771
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-03-14 11:04:11 +0300

    Initial Commit

---
+++ XyloPhone_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XyloPhone_app
+//
+//  Created by AYTAÇ BULANIK on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XyloPhone_appUITests/XyloPhone_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XyloPhone_appUITestsLaunchTests.swift
+//  XyloPhone_appUITests
+//
+//  Created by AYTAÇ BULANIK on 14.03.2022.
+//
+
+import XCTest
+
+class XyloPhone_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
