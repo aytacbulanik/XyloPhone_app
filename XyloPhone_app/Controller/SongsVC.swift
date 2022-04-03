21ff57b38d580f233fec932d7b91f0a9e5067907
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-04-03 15:29:33 +0300

    songsVC tableView and options complited

--- XyloPhone_app/Controller/SongsVC.swift
+++ XyloPhone_app/Controller/SongsVC.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class SongsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
     
 
+}
+
+extension SongsVC : UITableViewDelegate , UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Deneme"
+        return cell
+    }
 }
