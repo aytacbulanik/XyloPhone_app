4fc59f0fe4c8625bb128544fa053b621cc2f8bad
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-04-03 15:17:02 +0300

    songs vc created

---
+++ XyloPhone_app/Controller/SongsVC.swift
@@ -0,0 +1,21 @@
+//
+//  SongsVC.swift
+//  XyloPhone_app
+//
+//  Created by AYTAÇ BULANIK on 3.04.2022.
+//
+
+import UIKit
+
+class SongsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
