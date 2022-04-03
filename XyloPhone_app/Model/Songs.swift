2eda45df384ed2e6dc2721c1b21fffb219748058
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-04-03 20:36:17 +0300

    songs model created

---
+++ XyloPhone_app/Model/Songs.swift
@@ -0,0 +1,19 @@
+//
+//  Songs.swift
+//  XyloPhone_app
+//
+//  Created by AYTAÇ BULANIK on 3.04.2022.
+//
+
+import Foundation
+
+struct Songs {
+    var songName : String
+    var createDate : Date
+    var songAcords : [String]
+    var songTime : Float
+    
+    func singSong(song : [String]) {
+        
+    }
+}
