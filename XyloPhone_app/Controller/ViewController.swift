2e104a567036fc1f1d8ca2a395c5db8a64c00c5a
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-05-15 21:14:15 +0300

    AVAudio redorder added

--- XyloPhone_app/Controller/ViewController.swift
+++ XyloPhone_app/Controller/ViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate {
 
     var songBrain = SongBrain()
     var player : AVAudioPlayer!
+    var recordingSession : AVAudioSession!
+    var audioRecorder : AVAudioRecorder!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                    DispatchQueue.main.async {
+                        if allowed {
+                           // self.loadRecordingUI()
+                        } else {
+                            // failed to record!
+                        }
+                    }
+                }
+        }catch {
+            print(error.localizedDescription)
+        }
     }
 
    
@@ -26,6 +42,40 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func soundRecordButton(_ sender: UIButton) {
+        func recordTapped() {
+            if audioRecorder == nil {
+               // startRecording()
+            } else {
+               // finishRecording(success: true)
+            }
+        }
+    }
     
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+           // recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+          //  finishRecording(success: false)
+        }
+    }
 }
 
